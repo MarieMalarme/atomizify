bd02c1c8ab03e6170ea0061d6f8055174f82465e
Marie Malarme <malarmemarie@gmail.com>
2020-08-04 12:07:08 +0100

    Put the shared `blend_modes` values in a `_values.js` file in the properties > theming folder.

---
+++ properties/theming/_values.js
@@ -0,0 +1,18 @@
+export const blend_modes = [
+  'normal',
+  'multiply',
+  'screen',
+  'overlay',
+  'darken',
+  'lighten',
+  'color-dodge',
+  'color-burn',
+  'hard-light',
+  'soft-light',
+  'difference',
+  'exclusion',
+  'hue',
+  'saturation',
+  'color',
+  'luminosity',
+]
