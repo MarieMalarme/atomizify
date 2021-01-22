a25356d786798594d6e9b2bff3b3330f6f48267c
Marie Malarme <malarmemarie@gmail.com>
2021-01-22 19:57:30 +0000

    Made a generic function `generate_filters`.

--- functions/filters.js
+++ functions/filters.js
@@ -1,36 +1,23 @@
-import { assign_map } from './toolbox.js'
+import { assign_map, entries, assign_values } from './toolbox.js'
+import { filters } from '../variables/filters.js'
 
 export const generate_filters = ({ backdrop } = false) => {
   const prefix = backdrop ? `bdf` : `f`
   const selector = `${backdrop ? 'backdrop-' : ''}filter`
 
-  return {
-    ...assign_map(21, (i) => ({
-      [`${prefix}-blur${i}`]: `${selector}: blur(${i}px)`,
-    })),
-    ...assign_map(21, (i) => ({
-      [`${prefix}-bright${i * 25}`]: `${selector}: brightness(${i * 25}%)`,
-    })),
-    ...assign_map(21, (i) => ({
-      [`${prefix}-contrast${i * 10}`]: `${selector}: contrast(${i * 10}%)`,
-    })),
-    ...assign_map(11, (i) => ({
-      [`${prefix}-gray${i * 10}`]: `${selector}: grayscale(${i * 10}%)`,
-    })),
-    ...assign_map(36, (i) => ({
-      [`${prefix}-hue${i * 10}`]: `${selector}: hue-rotate(${i * 10}deg)`,
-    })),
-    ...assign_map(11, (i) => ({
-      [`${prefix}-invert${i * 10}`]: `${selector}: invert(${i * 10}%)`,
-    })),
-    ...assign_map(11, (i) => ({
-      [`${prefix}-opacity${i * 10}`]: `${selector}: opacity(${i * 10}%)`,
-    })),
-    ...assign_map(21, (i) => ({
-      [`${prefix}-saturate${i * 10}`]: `${selector}: saturate(${i * 10}%)`,
-    })),
-    ...assign_map(11, (i) => ({
-      [`${prefix}-sepia${i * 10}`]: `${selector}: sepia(${i * 10}%)`,
-    })),
-  }
+  const generated_filters = entries(filters).map(([name, value]) => {
+    const { suffix, unit, inc, repeat } = value
+    const filter = suffix || name
+    const filter_entries = assign_map(repeat, i => {
+      const multiplier = i * (inc || 10)
+      const key = `${prefix}-${name}${multiplier}`
+      const css_rule = `${selector}: ${filter}(${multiplier}${unit || '%'})`
+      return {
+        [key]: css_rule,
+      }
+    })
+    return filter_entries
+  })
+
+  return assign_values(generated_filters)
 }
---
+++ variables/filters.js
@@ -0,0 +1,11 @@
+export const filters = {
+  blur: { repeat: 21, inc: 1, unit: 'px' },
+  bright: { repeat: 21, inc: 25, suffix: 'brightness' },
+  contrast: { repeat: 21 },
+  grayscale: { repeat: 11, suffix: 'gray' },
+  hue: { repeat: 36, suffix: 'hue-rotate', unit: 'deg' },
+  invert: { repeat: 11 },
+  opacity: { repeat: 11 },
+  saturate: { repeat: 21 },
+  sepia: { repeat: 11 },
+}
